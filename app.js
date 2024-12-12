cf6e596da1e62ed0c56ba0cf128d059fb1cd8cd1
kirtiraj22 <kirtirajthakor064@gmail.com>
2024-12-12 16:42:54 +0530

    user auth

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
 require("dotenv").config();
 
 const express = require("express")
-const http = require("http")
+const http = require("http");
+const connectDB=require("./config/connect");
 
 const app = express();
 app.use(express.json());
+const server = http.createServer(app)
+
+const main = async () => {
+    try{
+        server.listen(process.env.PORT || 3000,"0.0.0.0",async () => {
+            console.log(`HTTP server is running on port http://localhost:${process.env.PORT || 3000}`) 
+        })
+
+        await connectDB(process.env.MONGO_URI).then((data) => {
+            console.log(`Mongodb is connected at ${data?.connection?.host}`)
+        })
+    }catch(error){
+        console.log(error);
+    }
+}
+
+main();
