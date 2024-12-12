f74cbe02287c05a41f2004af064bf33d2833af1d
kirtiraj22 <kirtirajthakor064@gmail.com>
2024-12-12 18:38:07 +0530

    calculate distance and fare

---
+++ utils/mapUtils.js
@@ -0,0 +1,87 @@
+// Haversine formula to calculate shortest distance
+const calculateDistance = (lat1, lon1, lat2, lon2) => {
+	// Earth's Radius
+	const R = 6371;
+
+	// convert latitude and longitude to radians
+	const dLat = (lat2 - lat1) * (Math.PI / 180);
+	const dLon = (lon2 - lon1) * (Math.PI / 100);
+
+	// calculate great-circle distance between two points
+	const a =
+		Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+		Math.cos(lat1 * (Math.PI / 180)) *
+			Math.cos(lat2 * (Math.PI / 180)) *
+			Math.sin(dLon / 2) *
+			Math.sin(dLon / 2);
+
+    // central angle between two points 
+    const c = 2  * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    // multiply with earth's radius
+    return R * c;
+};
+
+const calculateFare = (distance) => {
+    const rateStructure = {
+        bike : {
+            baseFare: 10,
+            perKmRate: 5,
+            minimumFare: 25
+        },
+        auto: {
+            baseFare: 15,
+            perKmRate: 7,
+            minimumFare: 30
+        },
+        cabEconomy: {
+            baseFare: 20,
+            perKmRate: 10,
+            minimumFare: 50
+        },
+        cabPremium: {
+            baseFare: 30,
+            perKmRate: 15,
+            minimumFare: 70
+        }
+    }
+
+    const fareCalculation = (baseFare, perKmRate, minimumFare) => {
+        // base fare is the fixed starting amount charged for every ride
+        const calculatedFare = baseFare + distance * perKmRate;
+        return Math.max(calculatedFare, minimumFare)
+    }
+
+    return {
+        bike: fareCalculation(
+            rateStructure.bike.baseFare,
+            rateStructure.bike.perKmRate,
+            rateStructure.bike.minimumFare
+        ),
+        auto: fareCalculation(
+            rateStructure.auto.baseFare,
+            rateStructure.auto.perKmRate,
+            rateStructure.auto.minimumFare
+        ),
+        cabEconomy: fareCalculation(
+            rateStructure.cabEconomy.baseFare,
+            rateStructure.cabEconomy.perKmRate,
+            rateStructure.cabEconomy.minimumFare
+        ),
+        cabPremium: fareCalculation(
+            rateStructure.cabPremium.baseFare,
+            rateStructure.cabPremium.perKmRate,
+            rateStructure.cabPremium.minimumFare
+        )
+    }
+}
+
+const generateOTP = () => {
+    return Math.floor(1000 + Math.random() * 9000).toString();
+}
+
+
+module.exports = {
+    calculateDistance,
+    calculateFare,
+    generateOTP
+}
